eaaafbcf3f6af57ed2968b8aa5af60764f505614
Peter <peterkilungya1@gmail.com>
2022-08-29 10:21:51 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+	// To pass the tests, don't forget to return your fetch!
+	let books = fetch("https://anapioficeandfire.com/api/books")
+		.then((res) => res.json())
+		.then(renderBooks);
+	return books;
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+	const main = document.querySelector("main");
+	books.forEach((book) => {
+		const h2 = document.createElement("h2");
+		h2.innerHTML = book.name;
+		main.appendChild(h2);
+	});
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener("DOMContentLoaded", function () {
+	fetchBooks();
 });
